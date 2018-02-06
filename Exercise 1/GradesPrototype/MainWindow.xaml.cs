2bf944c31759ab6a281cde10431de65ce78a0f25
Niek Keunegit config -- global user.email "Niek.Keune@hva.nl"git clone git@gitlab.dmci.hva.nl:info/git4bit.gitgit config --global user.name "Niek Keune <niekkeune@gmail.com>
2018-02-06 14:12:09 +0100

    Exercise 1
    Moet nog meer commentaar bij geleverd worden.

---
+++ Exercise 1/GradesPrototype/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+using GradesPrototype.Views;
+
+namespace GradesPrototype
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GotoLogon();
+        }
+
+        #region Navigation
+        public void GotoLogon()
+        {
+            logonPage.Visibility = Visibility.Visible;
+            studentsPage.Visibility = Visibility.Collapsed;
+            studentProfile.Visibility = Visibility.Collapsed;
+        }
+
+        private void GotoStudentsPage()
+        {
+            studentsPage.Visibility = Visibility.Visible;
+            studentProfile.Visibility = Visibility.Collapsed;
+            studentsPage.Refresh();
+        }
+
+        public void GotoStudentProfile()
+        {
+            studentProfile.Visibility = Visibility.Visible;
+            studentsPage.Visibility = Visibility.Collapsed;
+            studentProfile.Refresh();
+        }
+        #endregion
+
+        #region Event Handlers
+        private void Logon_Success(object sender, EventArgs e)
+        {
+            logonPage.Visibility = Visibility.Collapsed;
+            gridLoggedIn.Visibility = Visibility.Visible;
+            Refresh();
+        }
+
+        // Handle logoff
+        private void Logoff_Click(object sender, RoutedEventArgs e)
+        {
+            // Hide all views apart from the logon page
+            gridLoggedIn.Visibility = Visibility.Collapsed;
+            studentsPage.Visibility = Visibility.Collapsed;
+            studentProfile.Visibility = Visibility.Collapsed;
+            logonPage.Visibility = Visibility.Visible;
+        }
+
+        // Handle the Back button on the Student page
+        private void studentPage_Back(object sender, EventArgs e)
+        {
+            GotoStudentsPage();
+        }
+
+        private void studentsPage_StudentSelected(object sender, StudentEventArgs e)
+        {
+            SessionContext.CurrentStudent = e.Child;
+            GotoStudentProfile();
+        }
+        #endregion
+
+        #region Display Logic
+
+        private void Refresh()
+        {
+            if (SessionContext.UserRole == Role.Student)
+            {
+                GotoStudentProfile();
+            }
+            else if (SessionContext.UserRole == Role.Teacher)
+            {
+                GotoStudentsPage();
+            }
+        }
+        #endregion
+    }
+}
---
+++ Exercise 1/GradesPrototype/Views/StudentProfile.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Views
+{
+    /// <summary>
+    /// Interaction logic for StudentProfile.xaml
+    /// </summary>
+    public partial class StudentProfile : UserControl
+    {
+        public StudentProfile()
+        {
+            InitializeComponent();
+        }
+
+        #region Event Members
+        public event EventHandler Back;
+        #endregion
+
+        #region Events
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            // If the user is not a teacher, do nothing
+            if (SessionContext.UserRole != Role.Teacher)
+            {
+                return;
+            }
+
+            // If the user is a teacher, raise the Back event
+            // The MainWindow page has a handler that catches this event and returns to the Students page
+            if (Back != null)
+            {
+                Back(sender, e);
+
+                new Random().Next();
+            }
+        }
+        #endregion
+
+        public void Refresh()
+        {
+            Match matchNames = Regex.Match(SessionContext.CurrentStudent, @"([^ ]+) ([^ ]+)");
+
+            if (matchNames.Success)
+            {
+                string studentFirstName = matchNames.Groups[1].Value;       //Have to start at [1] because [0] gives the full name, for some reason. Anything after 2 results into a blank result.
+                string studentLastName = matchNames.Groups[2].Value;
+
+                ((TextBlock)studentName.Children[0]).Text = studentFirstName;
+                ((TextBlock)studentName.Children[1]).Text = studentLastName;
+            }
+
+            if (SessionContext.UserRole == Role.Student)
+            {
+                btnBack.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
